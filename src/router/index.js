7f7030a8a8681eb915cc6bef1c24fa4aff32788b
Juhee <juhee.baek@fitogether.com>
2020-08-23 01:51:11 +0900

    flex 레이아웃연습

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Input from "../views/training/Input.vue";
 
 Vue.use(VueRouter);
 
@@ -11,13 +12,12 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/input",
+    name: "Input",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: Input
   }
 ];
 
---
+++ src/views/training/Input.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="board__container">
+    <div class="board__content">
+      <div class="first__box">
+        <div class="box__field"></div>
+        <div class="box__actions"></div>
+      </div>
+      <div class="second__box"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.board__container {
+  width: 100%;
+  padding: 12px;
+  margin-right: auto;
+  margin-left: auto;
+  .board__content {
+    display: flex;
+    flex-wrap: wrap;
+    flex: 1 1 auto;
+    // margin-right: -12px;
+    // margin-left: -12px;
+    height: 800px;
+    .first__box {
+      display: flex;
+      flex-wrap: wrap;
+      flex: 1 1 auto;
+      background: hotpink;
+      padding: 10px;
+      flex: 0 0 70%;
+      max-width: 70%;
+      .box__field {
+        flex: 0 0 70%;
+        max-width: 70%;
+        background: greenyellow;
+      }
+      .box__actions {
+        flex: 0 0 30%;
+        max-width: 30%;
+        background: grey;
+      }
+    }
+    .second__box {
+      background: skyblue;
+      flex: 0 0 25%;
+      max-width: 25%;
+    }
+  }
+}
+
+/* 
+   - xs : mobile
+  - sm : ipad 세로 ( portrait )
+  - md : ipad 가로 ( landscape ), ipad pro<12.9> 세로 ( portrait )  [ css - media query  ]
+  - lg : mac 15, mac 13, ipad pro<12.9> 가로 ( landscape )
+  - xl : desktop 
+*/
+$sm-breakpoint: 600px;
+$md-breakpoint: 960px;
+$lg-breakpoint: 1264px;
+$xl-breakpoint: 1904px;
+
+@media screen and (max-width: $sm-breakpoint - 1) {
+  .board__container {
+    .board__content {
+      .first__box {
+        flex: 0 0 100%;
+        max-width: 100%;
+        .box__field {
+          flex: 0 0 100%;
+          max-width: 100%;
+        }
+        .box__actions {
+          flex: 0 0 100%;
+          max-width: 100%;
+        }
+      }
+      .second__box {
+        flex: 0 0 100%;
+        max-width: 100%;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: $md-breakpoint - 1) {
+}
+@media screen and (max-width: $lg-breakpoint - 1) {
+}
+@media screen and (max-width: $xl-breakpoint - 1) {
+}
+</style>
